3856f11acd95b65ac27048d46ab103bda162a73a
Juan Medina <medina189153@gmail.com>
2022-01-28 01:30:35 -0300

    improoving header look&feel

--- cafsite/src/components/Header.js
+++ cafsite/src/components/Header.js
@@ -1,28 +1,34 @@
+import { useState } from 'react';
 import '../assets/scss/components/header.scss'
+import CAF from '../assets/img/CAF.png'
 
 import { Link } from 'react-router-dom';
 
 const Header = () => {
+  const [currentTab, setCurrentTab] = useState('welcome');
+
+  const horizontalLine = (tabName) => {
+    if (tabName === currentTab) {
+      return (
+        <hr></hr>
+      )
+    }
+  }
+
   return (
     <div className="header--title">
       <div className="header--title__logo">
-        <h1><i>CAF</i></h1>
+        <img src={CAF} alt="caf logo" />
       </div>
       <div className="header--title__options">
         <Link to='/'>
-          <span>
-            Welcome
-          </span>
+          <button onClick={() => setCurrentTab('welcome')} > Welcome {horizontalLine('welcome')} </button>
         </Link>
         <Link to='/players'>
-          <span>
-            Players
-          </span>
+          <button onClick={() => setCurrentTab('players')} >Players {horizontalLine('players')}</button>
         </Link>
         <Link to='/newplayer'>
-          <span>
-            NewPlayer
-          </span>
+          <button onClick={() => setCurrentTab('newPlayer')} >New Player {horizontalLine('newPlayer')}</button>
         </Link>
       </div>
     </div>
