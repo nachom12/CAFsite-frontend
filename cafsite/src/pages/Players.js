fe9408f016cddac20ac7eb56872786e2a26e8517
Juan Medina <medina189153@gmail.com>
2022-04-07 14:18:11 -0300

    adding style to players card

--- cafsite/src/pages/Players.js
+++ cafsite/src/pages/Players.js
@@ -95,7 +95,7 @@ const Players = () => {
     return (
       <div className="loading">
         <h1>Loading..</h1>
-        <div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
+        <div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
       </div>
     )
   }
